7aa5a59692bc0376ca148016e493ae96190ea368
artacab <artacab@gmail.com>
2020-08-04 20:06:15 +0300

    переадресация

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const bot = new TelegramBot(TOKEN, {
         }
     }
 })
-
-
-bot.on('message', msg => {
+/* bot.on('message', msg => {
 
     const chatId = msg.chat.id
     bot.sendMessage(chatId, 'Inline keyboard', {
@@ -38,4 +36,69 @@ bot.on('message', msg => {
 bot.on('callback_query', query => {
     //bot.sendMessage(query.message.chat.id, debug(query))
     bot.answerCallbackQuery(query.id, `${query.data}`)
+}) */
+/* bot.on('inline_query', query => {
+    const results = []
+
+    for (let i = 0; i < 5; i++) {
+        results.push({
+            type: 'article',
+            id: i.toString(),
+            title: 'Title' + i,
+            input_message_content: {
+                message_text: `Article #${i+1}`
+            }
+        })
+    }
+    bot.answerInlineQuery(query.id, results, {
+        cache_time: 0
+    })
+}) */
+
+const inline_keyboard = [
+    [
+        {
+            text: 'reply',
+            callback_data: 'reply'
+        },
+        {
+            text: 'forward',
+            callback_data: 'forward'
+        }
+    ],
+    [
+        {
+            text: 'edit',
+            callback_data: 'edit'
+        },
+        {
+            text: 'delete',
+            callback_data: 'delete'
+        }
+    ]
+]
+
+
+
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+    const chatId = msg.chat.id
+    bot.sendMessage(chatId, 'Keyboard', {
+        reply_markup: {
+            inline_keyboard
+        }
+    })
+})
+
+bot.on('callback_query', query => {
+
+    const {chat, message_id, text} = query.message
+    switch(query.data) {
+        case 'forward':
+            bot.forwardMessage(chat.id, chat.id, message_id)
+            break
+    }
+    bot.answerCallbackQuery({
+        callback_query_id: query.id
+    })
 })
